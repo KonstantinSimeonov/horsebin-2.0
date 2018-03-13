5b69a00cb08fb119b8b1d733be004597719f8875
Konstantin Simeonov <kon.simeonov@protonmail.com>
2018-03-13 21:13:38 +0200

    Continuing the streak of awful code

--- api/src/Handler/Folders.hs
+++ api/src/Handler/Folders.hs
@@ -1,23 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Handler.Folders where
 
     import Import hiding ((.), id)
+    import Data.Either
+    import Data.Maybe
+    import Model
+    import Data.Aeson
+    -- import        Network.HTTP.Types       (status200, status400)
+    import           Network.Wai              (Application, Response, responseLBS)
 
     import qualified Data.Text.Read as Read
+    import Data.Text as T
+
+    isBadPath path = let (_, suffix) = T.breakOn "../" path
+                     in not $ T.null suffix
 
     getFoldersR :: Handler Value
     getFoldersR = do
-        pageNumber <- getIntParam "page"
-        pageSize <- getIntParam "size"
-        Import.print pageNumber
-        Import.print pageSize
+        pageNumber <- getIntParam "page" 0
+        pageSize <- getIntParam "size" 10
         folders <- runDB $ selectList [] [LimitTo pageSize, OffsetBy (pageSize * pageNumber)]
         returnJson (folders :: [Entity Folder])
         where
-            getIntParam paramName = (either (const (0 :: Int)) fst . Read.decimal . fromMaybe "0") <$> lookupGetParam paramName
-    
+            getIntParam paramName defaultValue = (fst . fromRight (defaultValue, "") . Read.decimal . fromMaybe "0") <$> lookupGetParam paramName
+
     postFoldersR :: Handler Value
     postFoldersR = do
-        body <- requireJsonBody :: Handler Folder
-        folderId <- runDB $ insert body
-        returnJson folderId
+        folder@(Folder contents) <- requireJsonBody :: Handler Folder
+        Import.print contents
+        let hasBadPaths = Import.any (isBadPath . fromMaybe "./" . pastePath) contents
+        folderId <- runDB $ insert folder
+        if hasBadPaths then do
+                            sendStatusJSON status400 $ object ["message" .= ("Bad path in paste" :: T.Text)]
+                       else returnJson folderId
